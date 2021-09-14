0d74805e5fff70a877b81205cab0f3e405508b06
Qspc <shibgotalloh_qqq@apps.ipb.ac.id>
2021-09-14 23:10:39 +0700

    login

---
+++ resources/views/create.blade.php
@@ -0,0 +1,10 @@
+{{-- @dd($post); --}}
+
+@extends('layouts.main')
+
+@section('container')
+    <h1>Create new Post</h1>
+
+
+    
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,16 +4,24 @@
 
 @section('container')
 
-  <h1 class="mt-8 ml-8 text-3xl font-bold">Daftar kosakata SANG</h1>
+<div class="ml-8">
+
+  <h1 class="mt-8  text-3xl font-bold">Daftar kosakata SANG</h1>
   
   @foreach ($posts as $post)
-      <article class="mt-8 ml-8">
+      <article class="mt-8">
         <h1 > <span class="font-bold">{{ $post->id }}.</span>   
           <a class="text-xl text-blue-700" href="posts/{{ $post->slug }}">{{ $post->title }}</a></h1>
         <h1>{{ $post->author }}</h1>
         <h1>{{ $post->script }}</h1>
       </article>
   @endforeach
+
+  <div class="mt-8 ml-4"><a class="text-1xl p-2 rounded-sm bg-blue-600 text-wh font-bold" 
+    href="/posts/create">Buat Form baru</a>
+  </div>
+
+</div>
 @endsection
 
 {{-- Post::create([
