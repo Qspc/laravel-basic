3a193a5f34345e597c5217db73a7363a7998cca8
Qspc <shibgotalloh_qqq@apps.ipb.ac.id>
2021-09-15 20:07:43 +0700

    login

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -3,16 +3,26 @@
 
 
 @section('container')
-    <h1>Post category : {{ $category }}</h1>
+  <div class="ml-8">
+
+    <h1 class="mt-8  text-3xl font-bold">List Category : {{ $title }}</h1>
+    
+    @foreach ($posts as $post)
+        <article class="mt-8">
+          <h1 > <span class="font-bold">{{ $post->id }}.</span>   
+            <a class="text-xl text-blue-700" href="posts/{{ $post->slug }}">{{ $post->title }}</a></h1>
+          <h1>{{ $post->script }}</h1>
+        </article>
+    @endforeach
+
+    <a class="text-sm mt-4" href="/home">back to <span class="text-blue-700">home</span> </a>
+
   
-  @foreach ($posts as $post)
-      <article class="mb-4 mx-4">
-        <h1>
-          <a href="posts/{{ $post->slug }}">{{ $post->title }}</a></h1>
-        <h1>{{ $post->author }}</h1>
-        <h1>{{ $post->script }}</h1>
-      </article>
-  @endforeach
+    <div class="mt-8 ml-4"><a class="text-1xl p-2 rounded-sm bg-blue-600 text-wh font-bold" 
+      href="/posts/create">Buat Form baru</a>
+    </div>
+  
+  </div>
 @endsection
 
 {{-- Post::create([
