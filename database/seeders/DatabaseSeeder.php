4e7affe78b94f8ebd3a304f2451700eb8fe3c156
Qspc <shibgotalloh_qqq@apps.ipb.ac.id>
2021-09-17 22:39:04 +0700

    login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,73 +17,74 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(5)->create();
+        // User::factory(5)->create();
+        \App\Models\User::factory(10)->create();
 
-        User::create([
-            'name' => "qspc",
-            'email' => "qspc@gmail.com",
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => "qspc",
+        //     'email' => "qspc@gmail.com",
+        //     'password' => bcrypt('12345')
+        // ]);
 
-        User::create([
-            'name' => "spidpes",
-            'email' => "spidpes@gmail.com",
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => "spidpes",
+        //     'email' => "spidpes@gmail.com",
+        //     'password' => bcrypt('12345')
+        // ]);
 
-        category::create([
-            'name' => "film",
-            'slug' => "film"
-        ]);
+        // category::create([
+        //     'name' => "film",
+        //     'slug' => "film"
+        // ]);
 
-        category::create([
-            'name' => "non film",
-            'slug' => "non-film"
-        ]);
+        // category::create([
+        //     'name' => "non film",
+        //     'slug' => "non-film"
+        // ]);
 
-        Post::create([
-            'title' => "sang pencerah",
-            'slug' => "sang-pencerah",
-            'script' => "lorem ipsum",
-            'body' => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, dolor.",
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => "sang pencerah",
+        //     'slug' => "sang-pencerah",
+        //     'script' => "lorem ipsum",
+        //     'body' => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, dolor.",
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => "sang kuriang",
-            'slug' => "sang-kuriang",
-            'script' => "lorem ipsum",
-            'body' => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, dolor.",
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => "sang kuriang",
+        //     'slug' => "sang-kuriang",
+        //     'script' => "lorem ipsum",
+        //     'body' => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, dolor.",
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => "sang sakala",
-            'slug' => "sang-sakala",
-            'script' => "lorem ipsum",
-            'body' => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, dolor.",
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => "sang sakala",
+        //     'slug' => "sang-sakala",
+        //     'script' => "lorem ipsum",
+        //     'body' => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, dolor.",
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => "sang sang lampu",
-            'slug' => "sang-sang-lampu",
-            'script' => "lorem ipsum",
-            'body' => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, dolor.",
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => "sang sang lampu",
+        //     'slug' => "sang-sang-lampu",
+        //     'script' => "lorem ipsum",
+        //     'body' => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, dolor.",
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => "sang penakluk",
-            'slug' => "sang-penakluk",
-            'script' => "lorem ipsum",
-            'body' => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, dolor.",
-            'category_id' => 1,
-            'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => "sang penakluk",
+        //     'slug' => "sang-penakluk",
+        //     'script' => "lorem ipsum",
+        //     'body' => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, dolor.",
+        //     'category_id' => 1,
+        //     'user_id' => 2
+        // ]);
     }
 }
